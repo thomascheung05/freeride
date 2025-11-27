e8ce89f608b954de24a4f0b83a5e59827d41ea36
thomascheung05 <thomascheung48@gmail.com>
2025-11-27 18:12:45 -0500

    tried to mac screen mirror work on pc but not much luck

--- main.py
+++ main.py
@@ -1,168 +1,93 @@
-import asyncio                      # Manage bluetoth notifications without blocking 
-from bleak import BleakClient       # type: ignore # BLE client 
-import struct                       # parse raw bytes into integers/floats using specified binary formats
+import ctypes
+from ctypes import wintypes
+import win32gui
+import win32ui
 
 
 
 
-DEVICE_ID = "7CA0DD25-9E34-CF6F-3230-683A4A8974CD"          # ID of device
-CHAR_UUID = "00002A63-0000-1000-8000-00805f9b34fb"          # BLE characteristic UUID to subsribe to, 00002A63 is cycling power  
-WHEEL_CIRCUMFERENCE = 2.105                         # Bike wheel circumference
-prev_crank_revs = None
-prev_crank_event_time = None
-prev_wheel_revs = None
-prev_wheel_event_time = None
+def capture_window(filename="window_capture.png"):
+    hwnd = '329678'
+    # hwnd = win32gui.FindWindow(None, title)
+    if not hwnd:
+        print("Window not found!")
+        return
+
+    # Get window size
+    left, top, right, bottom = win32gui.GetClientRect(hwnd)
+    width = right - left
+    height = bottom - top
+
+    # Get device context
+    hwndDC = win32gui.GetWindowDC(hwnd)
+    mfcDC = win32ui.CreateDCFromHandle(hwndDC)
+    saveDC = mfcDC.CreateCompatibleDC()
+
+    bmp = win32ui.CreateBitmap()
+    bmp.CreateCompatibleBitmap(mfcDC, width, height)
+    saveDC.SelectObject(bmp)
+
+    # Call PrintWindow via ctypes
+    PW_RENDERFULLCONTENT = 0x00000002
+    ctypes.windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), PW_RENDERFULLCONTENT)
+
+    bmp.SaveBitmapFile(saveDC, filename)
+
+    # Free resources
+    win32gui.DeleteObject(bmp.GetHandle())
+    saveDC.DeleteDC()
+    mfcDC.DeleteDC()
+    win32gui.ReleaseDC(hwnd, hwndDC)
+
+    print(f"Captured {filename}")
+
+capture_window()
 
 
-def notification_handler_old(sender, data):
-    """This will be called every time the device sends a notification."""
-    print(data)
-
-async def fx_connect_to_device():
-    print("Trying to connect to KICKR SNAP...")
-
-    async with BleakClient(DEVICE_ID) as client:
-        if client.is_connected:
-            print("Connected to KICKR SNAP!")
-        else:
-            print("Failed to connect.")
 
 
-def notification_handler(sender, data): # sender is characteristic handle/ID given by bleak, data is the raw bytes we get from the machine
-    global prev_crank_revs, prev_crank_event_time, prev_wheel_revs, prev_wheel_event_time       # it will read and write variables in the global envs 
 
-    # Convert to bytes for parsing
-    b = bytearray(data)                                         # Convert the data we get from machine into bytearray meaning we can now index the bytes that the machine sends us 
-    
-    # Flags (2 bytes)
-    flags = struct.unpack_from("<H", b, 0)[0]                   # Reads the first two bytes as an Unsigned 16-bit integer, a number stored in 16 bit format that is only positive, this value tell us what data is available in the data packet being sent by the machine 
+# def list_letsview_windows():
+#     def enum_windows(hwnd, results):
+#         title = win32gui.GetWindowText(hwnd)
+#         if "LetsView" in title:
+#             results.append((hwnd, title))
+#     windows = []
+#     win32gui.EnumWindows(enum_windows, windows)
+#     return windows
+# for hwnd, title in list_letsview_windows():
+#     print(f"HWND: {hwnd}, Title: {title}")
 
-    # Instantaneous power (2 bytes, signed)
-    power_watts = struct.unpack_from("<h", b, 2)[0]             # Reads the next two bytes as Signed 16-bit integer, a number stored in 16 bit format that is positive or negative, the highest bit is the sign bit  
 
-    offset = 4                                                  # This is to keep track where we are in the data string from the machine, we used 2 bits for flag and two for power_watts so we are now at 4 
-    accumulated_torque = None                                   # set to none and only fills is the data is present 
-    crank_revs = None
-    last_crank_event_time = None
-    cadence_rpm = None
-    speed_kmh = None
 
-    # Check for optional fields based on flags
-    # if flags & 0x01:  # Pedal Power Balance present (skip for now)
-    #     pedal_balance = struct.unpack_from("<B", b, offset)[0]
-    #     offset += 1
 
-    # if flags & 0x02:  # This checks if accumulated torque is present in the data, if yes it unpacks it and moves the offset up 2
-    #     accumulated_torque = struct.unpack_from("<H", b, offset)[0] / 32.0  # Nm
-    #     offset += 2
 
-    if flags & 0x04:  # This checks if the wheel revolution data is present and if yes it unpacks it and moves the offest by 6
-        wheel_revs, last_wheel_event_time = struct.unpack_from("<LH", b, offset)
-        offset += 6
+# WINDOW_TITLE  = "LetsView"
+
+# def get_window(title):
+#     windows = gw.getWindowsWithTitle(title)
+#     if not windows:
+#         return None
+#     return windows[0]
+
+# def main():
+#     print("Looking for LetsView window...")
+#     while True:
+#         window = get_window(WINDOW_TITLE)
+#         if window is None:
+#             print("Window not found. Waiting...")
+#             time.sleep(2)
+#             continue
+
+#         # Capture the entire window
+#         left, top, width, height = window.left, window.top, window.width, window.height
+#         screenshot = pyautogui.screenshot(region=(left, top, width, height))
         
-        # Calculate speed
-        print("-" * 30)
-        print(f'Prev_wheel_revs:{prev_wheel_revs}')
-        print(f'prev_wheel_event_time:{prev_wheel_event_time}')
-        print("-" * 15)
-        print(f'wheel_revs:{wheel_revs}')
-        print(f'last_wheel_event_time:{last_wheel_event_time}')
-        print("-" * 30)
-        print(prev_wheel_revs, prev_wheel_event_time)
-        if prev_wheel_revs is not None and prev_wheel_event_time is not None:
-            delta_revs = wheel_revs - prev_wheel_revs
-            delta_time_sec = (last_wheel_event_time - prev_wheel_event_time) / 1024
-            if delta_time_sec > 0:
-                speed_m_s = (delta_revs * WHEEL_CIRCUMFERENCE) / delta_time_sec
-                speed_kmh = speed_m_s * 3.6
-        prev_wheel_revs = wheel_revs
-        prev_wheel_event_time = last_wheel_event_time # i think because last_wheel_event_time is such a mslal time number that it rounds it to seconds and its alays 0 making the speed value error (divide by 0) and causing it not to display, my theory for now
+#         # Save the screenshot
+#         screenshot.save("letsview_capture.png")
+#         print(f"Screenshot saved! Size: {width}x{height}")
+#         break  # stop after one capture
 
-    if flags & 0x08:  # Crank Revolution Data present
-        crank_revs, last_crank_event_time = struct.unpack_from("<HH", b, offset)
-        offset += 4
-        # Calculate cadence
-        if prev_crank_revs is not None and prev_crank_event_time is not None:
-            delta_revs = crank_revs - prev_crank_revs
-            delta_time_sec = (last_crank_event_time - prev_crank_event_time) / 1024
-            if delta_time_sec > 0:
-                cadence_rpm = (delta_revs / delta_time_sec) * 60
-        prev_crank_revs = crank_revs
-        prev_crank_event_time = last_crank_event_time
-
-    # Print all parsed variables
-    # print(f"Flags: {flags}")
-    # print(f"Power (W): {power_watts}")
-    # if accumulated_torque is not None:
-    #     print(f"Torque (Nm): {accumulated_torque}")
-    # if crank_revs is not None:
-    #     print(f"Crank Revs: {crank_revs}")
-    #     print(f"Last Crank Event Time: {last_crank_event_time}")
-    # if cadence_rpm is not None:
-    #     print(f"Cadence (RPM): {cadence_rpm:.2f}")
-    # if wheel_revs is not None:
-    #     print(f"Wheel Revs: {wheel_revs}")
-    #     print(f"Last Wheel Event Time: {last_wheel_event_time}")
-    # if speed_kmh is not None:
-    #     print(f"Speed (km/h): {speed_kmh:.2f}")
-    # print("-" * 30)
-
-
-async def fx_connect_to_device():
-    print("Trying to connect to KICKR SNAP...")
-
-    async with BleakClient(DEVICE_ID) as client:
-        if client.is_connected:
-            print("Connected to KICKR SNAP!")
-            # Subscribe to notifications
-            await client.start_notify(CHAR_UUID, notification_handler_old)
-            print("Subscribed to notifications. Waiting for data...")
-            
-            # Keep the program running to receive notifications
-            await asyncio.sleep(30)  # Adjust the duration as needed
-            await client.stop_notify(CHAR_UUID)
-        else:
-            print("Failed to connect.")
-
-# Run the async function
-asyncio.run(fx_connect_to_device())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# if __name__ == "__main__":
+#     main()
+    
